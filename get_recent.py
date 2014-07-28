b1560a95af20bc8121dac88d1b8acba1a278ccbe
Dmitry Shachnev <mitya57@gmail.com>
2014-07-28 09:34:45 +0400

    MgtRuBackend is no longer needed

--- get_recent.py
+++ get_recent.py
@@ -2,7 +2,6 @@
 # Author: 2014 Dmitry Shachnev <d-shachnev@yandex.ru>
 
 from mgtparser.backend_mgtorg import MgtOrgBackend
-from mgtparser.backend_mgtru import MgtRuBackend
 from mgtparser.common import RouteType, get_routes_list
 from argparse import ArgumentParser
 from datetime import datetime
@@ -17,26 +16,15 @@ parser.add_argument('-D', '--download', help='download the schedules as JSON fil
 parser.add_argument('-p', '--print-info', help='print short info about the schedule', action='store_true')
 parser.add_argument('-O', '--output-dir', help='directory to store downloaded schedules in', default='schedules')
 parser.add_argument('-g', '--debug', help='enable debug output', action='store_true')
-parser.add_argument('--ru', help='only search mosgortrans.ru site', action='store_true')
-parser.add_argument('--org', help='only search mosgortrans.org site', action='store_true')
 args = parser.parse_args()
 
 backend_org = MgtOrgBackend()
-backend_ru = MgtRuBackend()
 
-all_mgtorg_routes = map(backend_org.get_routes_by_type, RouteType)
-all_mgtru_routes = map(backend_ru.get_routes_by_type, RouteType)
-
-if args.ru:
-	all_mgtorg_routes = []
-if args.org:
-	all_mgtru_routes = []
+all_routes = map(backend_org.get_routes_by_type, RouteType)
 
 if args.debug:
-	all_mgtorg_routes = list(all_mgtorg_routes)
-	all_mgtru_routes = list(all_mgtru_routes)
-	print('All MgtOrgBackend routes: %r' % all_mgtorg_routes)
-	print('All MgtRuBackend routes: %r' % all_mgtru_routes)
+	all_routes = list(all_routes)
+	print('All routes: %r' % all_routes)
 
 routes_list = None
 if args.input_file:
@@ -100,30 +88,9 @@ def process_org(route_type, route):
 					'BA', schedule)
 				print_info(schedule)
 
-@retrying_wrapper
-def process_ru(route_type, route):
-	route_info = backend_ru.get_route_info(route_type, route)
-	if args.debug:
-		print('got route_info for', route_type.name, route)
-	for date_info in route_info:
-		if advanced_mode and check_timestamp(date_info[1]):
-			directions = len(backend_ru.get_directions(route_type,
-				route, date_info[0]))
-			for direction in (('AB', 'BA') if directions > 1 else ('AB',)):
-				schedule = backend_ru.get_schedule_from_route_info(
-					route_info, date_info[0], direction, None)
-				save_schedule(route_type, route, date_info[0],
-					direction, schedule)
-				print_info(schedule)
-		process_route(route_type, route, date_info[0], date_info[1])
-
 times_file = open(args.output_file, 'w')
-for route_type, routes in zip(RouteType, all_mgtorg_routes):
+for route_type, routes in zip(RouteType, all_routes):
 	for route in routes:
 		if (not routes_list) or (route_type, route) in routes_list:
 			process_org(route_type, route)
-for route_type, routes in zip(RouteType, all_mgtru_routes):
-	for route in routes:
-		if (not routes_list) or (route_type, route) in routes_list:
-			process_ru(route_type, route)
 times_file.close()
